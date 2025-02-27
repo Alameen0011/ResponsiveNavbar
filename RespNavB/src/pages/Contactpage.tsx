110e3c5e4eb005c2a5e6dd78a8b1de514e56d8ce
Al Ameen <sayedalameen1981@gmail.com>
2025-02-27 15:35:10 +0530

    working with different form Elements planning to use modern formdata

--- RespNavB/src/pages/Contactpage.tsx
+++ RespNavB/src/pages/Contactpage.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contactpage = () => {
-  console.log("forms created")
+
+  const [userName,setUsername] = useState<string>("")
+  const [email,setEmail] = useState<string>("")
+  const [isChecked,setIsChecked] = useState<boolean>(false)
+  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      console.log(e, ": available events")
+       setUsername(e.target.value)
+       return;
+  }
+
+  const handleSubmit  = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(userName)
+    console.log(email)
+  }
+
+
   return (
     <div>
-      Contact Page 
-      working on forms
+      <h2>Contact form</h2>
+      <form onSubmit={handleSubmit}  >
+        <label htmlFor="name"  >Name :</label>
+        <input 
+        type="text"
+        id='name'
+        name='name'
+        value={userName} 
+        onChange={handleChange }
+        />
+
+        <label htmlFor='email' >Email :   </label>
+        <input type="email" id='email' name="email" value={email} onChange={(e ) => setEmail(e.target.value) } />
+      
+
+        <label >please check 
+          <input type = "checkbox" checked= {isChecked} onChange={(e) => setIsChecked(e.target.checked)} />
+
+
+          </label>
+       
+        <p>Checkbox is {isChecked ? "Checked ✅" : "Unchecked ❌"}</p>
+        <button type="submit">click here</button>
+
+
+
+
+
+      </form>
+    
     </div>
   )
 }
